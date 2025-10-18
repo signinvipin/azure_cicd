2c7ed850d254f23fa6ec3910f1e332cb3881cfdc
signinvipin <signinvipin@gmail.com>
2025-10-18 21:38:24 +0530

    Add change to java content

--- src/main/java/com/levo/jenkinsCICDk8s/controller/HelloController.java
+++ src/main/java/com/levo/jenkinsCICDk8s/controller/HelloController.java
@@ -13,7 +13,7 @@ public class HelloController {
             <html>
               <body>
                 <h2>Hello! This is a test app.</h2>
-                <p>Congratulations on deploying Docker image successfully using Azure DevOps classic editor.</p>
+                <p>Congratulations on deploying Docker image successfully using Azure DevOps classic editor on Azure Kubernetes.</p>
                 <pre>
   /\\_/\\  
  ( o.o ) 
